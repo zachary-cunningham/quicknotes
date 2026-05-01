d3c2bc16670a0cb79182a04e33c36e5c265ab4f2
Zachary Cunningham <zach.b.cunningham@icloud.com>
2026-05-01 09:40:57 +0100

    nest in data attribute

--- quicknotes/serializers.py
+++ quicknotes/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from quicknotes.models import Note
 
 class NoteSerializer(ModelSerializer):
+	# this is API level validation, not DB or front-end
+	# content = serializers.CharField(required=False, allow_blank=True, default="")
 	class Meta:
 		model = Note
 		fields = ['id', 'title', 'content']
--- quicknotes/views.py
+++ quicknotes/views.py
@@ -1,9 +1,28 @@
 from django.http import HttpResponse, JsonResponse
+
+from quicknotes.serializers import NoteSerializer
 from .models import Note
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 def home(request):
 	return HttpResponse("Welcome Home!")
 
-def api_notes(request):
-	data = list(Note.objects.values())
-	return JsonResponse({'notes': data })
+# def api_notes(request):
+# 	data = list(Note.objects.values())
+# 	return JsonResponse({'notes': data })
+
+class NoteViewSet(ModelViewSet):
+	queryset = Note.objects.all()
+	serializer_class = NoteSerializer
+
+	# to get a list of data
+	def list(self, request, *args, **kwargs):
+		queryset = self.filter_queryset(self.get_queryset())
+		serializer = self.get_serializer(queryset, many=True)
+		return Response({"data": serializer.data})
+	
+	def retrieve(self, request, *args, **kwargs):
+		instance = self.get_object()
+		serializer = self.get_serializer(instance)
+		return Response({"data": serializer.data})
