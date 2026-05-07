0f25b6a0317091d40a30c69240e2c6a2c3e23f25
Zachary Cunningham <zach.b.cunningham@icloud.com>
2026-05-07 14:18:44 +0100

    freeze new installations to requirements.txt

--- quicknotes/settings.py
+++ quicknotes/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'quicknotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.sqlite3',
+# 		'NAME': BASE_DIR / 'db.sqlite3',
+# 	}
+# }
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+	"default": {
+		"ENGINE": "django.db.backends.postgresql",
+		"NAME": "postgres",
+		"USER": "postgres",
+		"PASSWORD": "password",
+		"HOST": "127.0.0.1",
+		"PORT": "5431",
 	}
 }
 
