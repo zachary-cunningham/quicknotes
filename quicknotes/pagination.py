47c8f60161b8cfe228aab620ea76b713fef9131d
Zachary Cunningham <zach.b.cunningham@icloud.com>
2026-05-22 12:51:16 +0100

    offset pagination demo

--- quicknotes/pagination.py
+++ quicknotes/pagination.py
@@ -1,10 +1,12 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 from rest_framework.response import Response
 
-class CustomPagination(PageNumberPagination):
+class CustomPagination(LimitOffsetPagination):
+	default_limit = 5
+	
 	def get_paginated_response(self, data):
 		return Response({
-			"count": self.page.paginator.count,
+			"count": self.count,
 			"next": self.get_next_link(),
 			"previous": self.get_previous_link(),
 			"data": data
